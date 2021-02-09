9de3de7f636df6e65552fa2ee41882bf7cb39359
onigari <50879694+proteekrosul@users.noreply.github.com>
2021-02-10 00:45:02 +0600

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,10 @@
+# by considering the terms in the Fibonacci sequence(starting with 1, 2) whose values do not exceed four million, find the sum of the even-valued terms.
+
 fib = [1,2]
+
 while fib[-1] < 4000000:
     fib.append(fib[-2]+fib[-1])
-fib=fib[:-1]
+
+fib = fib[:-1]
+
 print(sum([i for i in fib if i%2==0]))
